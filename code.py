6e4f20ab2c90fb0d446a33c9a86f00fefba405d0
HinesDevelopment <149387491+HinesDevelopment@users.noreply.github.com>
2023-10-31 08:35:48 -0400

    Update code.py

--- code.py
+++ code.py
@@ -12,13 +12,19 @@ form = cgi.FieldStorage()
 user_input_username = form.getvalue('username')
 user_input_password = form.getvalue('password')
 
-# Print the HTTP header
+# Print the HTTP header with a 200 OK status
 print("Content-type: text/html\n")
 
 # Check if the provided username and password match the expected values
 if user_input_username == expected_username and user_input_password == expected_password:
     # Successful login
     print(f"<h1>Welcome, {user_input_username}!</h1>")
+    
+    # Display a chat box panel
+    print("<div id='chat-panel'>")
+    print("<h2>Chat Panel</h2>")
+    # You can add more HTML content for the chat panel here
+    print("</div>")
 else:
     # Invalid login
     print("<h1>Invalid username or password. Please try again.</h1>")
